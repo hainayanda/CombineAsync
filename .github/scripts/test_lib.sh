5eb7a78e4d261c5359e8a076e37f7b7ae56effc0
Nayanda Haberty <hainayanda@outlook.com>
2023-05-25 10:18:25 +0700

    Update the scripts

--- .github/scripts/test_lib.sh
+++ .github/scripts/test_lib.sh
@@ -2,5 +2,5 @@ set -eo pipefail
 
 xcodebuild -workspace Example/CombineAsync.xcworkspace \
             -scheme CombineAsync-Example \
-            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=16.4,name=iPhone\ 14 \
             clean test | xcpretty
